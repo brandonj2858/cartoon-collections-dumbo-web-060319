959ad4cfc7955bcf82dc8613344c52bb041e0642
Brandon James <brandonj2858@gmail.com>
2019-05-23 14:09:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ end
 def find_the_cheese(flavor)
   cheese_types = ["cheddar", "gouda", "camembert"]
   flavor.find do |types|
-    types.include?(types)
+    cheese_types.include?(types)
 end
 end
